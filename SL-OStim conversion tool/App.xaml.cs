1cdaf1c0f200bd8c555e4d2ad7f9bd7b6e56b137
PrometheusSSE <lasse@herzoeglich.de>
2021-05-21 08:45:56 +0200

    Fixing minor bugs and implementing automation of *.xml folder path

--- SL-OStim conversion tool/App.xaml.cs
+++ SL-OStim conversion tool/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace SL_OStim_conversion_tool
+namespace OStimConversionTool
 {
     /// <summary>
     /// Interaction logic for App.xaml
