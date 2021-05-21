4741cee8a79342f5ea2ca873a17b199e86cd0c4c
PrometheusSSE <79590942+PrometheusSSE@users.noreply.github.com>
2021-05-21 09:48:41 +0200

    another Bug Fix

--- SL-OStim conversion tool/Startup.cs
+++ SL-OStim conversion tool/Startup.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.IO;
 using System.Windows;
 
@@ -37,7 +36,7 @@ namespace OStimConversionTool
             Ookii.Dialogs.Wpf.VistaFolderBrowserDialog folderBrowserDialog = new() { };
             {
                 if (folderBrowserDialog.ShowDialog() == true)
-            {
+                {
                     rootDir.Content = folderBrowserDialog.SelectedPath;
                 }
             }
@@ -46,24 +45,42 @@ namespace OStimConversionTool
         private void ConversionProcess_Click(object sender, RoutedEventArgs e)
         {
             var ls = (mN: moduleName.GetLineText(0), aC: animClass.GetLineText(0), aN: animName.GetLineText(0), rD: rootDir.Content.ToString());
+            var animDir = Path.Combine(ls.rD, @"meshes\0SA\mod\0Sex\anim\", ls.mN, ls.aC, ls.aN);
+            var xmlDir = Path.Combine(ls.rD, @"meshes\0SA\mod\0Sex\scene", ls.mN, ls.aC);
             var fnis_path = Path.Combine(ls.rD, @$"meshes\actors\character\animations\0Sex_{ls.mN}_A");
+            
+            if (!Directory.Exists(animDir))
+                Directory.CreateDirectory(animDir);
+
+            if (!Directory.Exists(xmlDir))
+            {
+                Directory.CreateDirectory(xmlDir);
+                File.WriteAllText(xmlDir + $@"\{ls.aN}", "");
+            }
+
             if (!Directory.Exists(fnis_path))
             {
                 Directory.CreateDirectory(fnis_path);
                 File.WriteAllText(fnis_path + @$"\FNIS_0Sex_{ls.mN}_A_List.txt", "");
             }
+
             for (int i = 0; i < lbFiles.Items.Count; i++)
             {
-                var ls = (mN: moduleName.GetLineText(0), aC: animClass.GetLineText(0), aN: animName.GetLineText(0), rD: rootDir.Content.ToString());
                 var oldName = lbFiles.Items[i].ToString();
-                var stage = Char.GetNumericValue(oldName[^5]) - 1;
-                var actor = Char.GetNumericValue(oldName[^8]) - 1;
+                var stage = char.GetNumericValue(oldName[^5]) - 1;
+                var actor = char.GetNumericValue(oldName[^8]) - 1;
                 var newName = $"0Sx{ls.mN}_{ls.aC}-{ls.aN}_S{stage}_{actor}.hkx";
-                var animDir = Path.Combine(ls.rD, @"meshes\0SA\mod\0Sex\anim\", ls.mN, ls.aC, ls.aN);
-                Directory.CreateDirectory(animDir);
+
                 File.Copy(Path.Combine(sourceDir, oldName), Path.Combine(animDir, newName));
-                File.AppendAllText(fnis_path + @$"\FNIS_0Sex_{ls.mN}_A_List.txt", @$"b -Tn {newName} ..\..\..\..\{animDir}\{newName}{Environment.NewLine}");
+                File.AppendAllText(fnis_path + @$"\FNIS_0Sex_{ls.mN}_A_List.txt", @$"b -Tn {Path.GetFileName(newName)} ..\..\..\..\{animDir}\{newName}{Environment.NewLine}");
             }
+
+            XmlScriber(xmlDir + $@"\{ls.aN}");
+            lbFiles.Items.Clear();
+        }
+
+        private void XmlScriber(string xmlPath)
+        {
         }
     }
 }
