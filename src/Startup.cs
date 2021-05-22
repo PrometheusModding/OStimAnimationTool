c93177e092758e794af35a33d45efe3c6edb7de9
PrometheusSSE <79590942+PrometheusSSE@users.noreply.github.com>
2021-05-22 08:30:39 +0200

    implements first prototy pe of automated .xml files

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Windows;
 
@@ -21,15 +21,14 @@ namespace OStimConversionTool
                 Filter = "Havok Animation files (*.hkx)|*hkx|All files (*.*)|*.*",
                 InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
             };
-            
+
             if (openFileDialog.ShowDialog() != true) return;
-            
+
             _sourceDir = openFileDialog.FileName;
             _sourceDir = _sourceDir.Remove(_sourceDir.Length - openFileDialog.SafeFileName.Length);
-            
+
             foreach (string filename in openFileDialog.FileNames)
                 LbFiles.Items.Add(Path.GetFileName(filename));
-            MessageBox.Show($"Source Dir is {_sourceDir}");
         }
 
         private void ChooseRootDir_Click(object sender, RoutedEventArgs e)
@@ -58,7 +57,7 @@ namespace OStimConversionTool
                 throw new NotImplementedException();
             if (rootDir == null)
                 throw new NotImplementedException();
-            
+
             var animDir = Path.Combine(rootDir, @"meshes\0SA\mod\0Sex\anim\", moduleName, animClass, animName);
             var xmlDir = Path.Combine(rootDir, @"meshes\0SA\mod\0Sex\scene", moduleName, animClass);
             var fnisPath = Path.Combine(rootDir, @$"meshes\actors\character\animations\0Sex_{moduleName}_A");
@@ -69,7 +68,7 @@ namespace OStimConversionTool
             if (!Directory.Exists(xmlDir))
             {
                 Directory.CreateDirectory(xmlDir);
-                File.WriteAllText(Path.Combine(xmlDir, $"{animName}"), "");
+                File.WriteAllText(Path.Combine(xmlDir, $"{animName}.xml"), "");
             }
 
             if (!Directory.Exists(fnisPath))
@@ -87,20 +86,49 @@ namespace OStimConversionTool
                 var actor = 0;
                 if (Math.Abs(char.GetNumericValue(oldName[^8]) - 1) < double.Epsilon)
                     actor = 1;
-                
+
                 var newName = $"0Sx{moduleName}_{animClass}-{animName}_S{stage}_{actor}.hkx";
-                
+
                 File.Copy(Path.Combine(_sourceDir, oldName), Path.Combine(animDir, newName));
                 var contents = @$"b -Tn {Path.GetFileName(newName)} ..\..\..\..\{animDir}\{newName}{Environment.NewLine}";
                 File.AppendAllText(Path.Combine(fnisPath, $"FNIS_0Sex_{moduleName}_A_List.txt"), contents);
             }
-            
-            XmlScriber(Path.Combine(xmlDir, $"{animName}"));
+
+            XmlScriber(Path.Combine(xmlDir, $"{animName}.xml"));
             LbFiles.Items.Clear();
         }
 
-        private static void XmlScriber(string xmlPath)
+        private void XmlScriber(string xmlPath)
         {
+            var moduleName = ModuleName.GetLineText(0);
+            var animClass = AnimClass.GetLineText(0);
+            var animName = AnimName.GetLineText(0);
+            var animID = $"0Sx{moduleName}_{animClass}-{animName}";
+
+            File.AppendAllText(xmlPath, string.Format(
+                @$"<scene id=""{moduleName}||{animClass}|{animName}"" actors=""2"" style=""Oscene"">
+<info name="""" animator=""""/>
+<anim id=""{animID}_S1"" t=""L"" l=""2""/>"));
+
+            for (int i = 0; i < LbFiles.Items.Count / 2; i++)
+            {
+                var speedUnit = "^idle";
+                if (i > 0)
+                    speedUnit = "^thrustsPerSec";
+
+                File.AppendAllText(xmlPath, string.Format(
+@$"<speed a=""0"" min=""0"" max=""{LbFiles.Items.Count / 2}"" name=""thrusts"">
+    <sp mtx = ""{speedUnit}"" tar = ""1"" type = """" >
+        <anim id=""{animID}_sourceDir{i}"" t=""L"" l=""2"" i0=""{i}"" i1=""{i}"""));
+
+                if (i > 0)
+                    File.AppendAllText(xmlPath, string.Format(
+$@"<ae evty=""sexThrustR"" whodid=""0"" tar=""1"" ori=""pussy"" thrust=""cock"" sound=""pussy"" soundpow=""0""
+imp=""ass"" impside=""R"" impforce=""2"" impdmg="".01"" thrustforce=""2"" oridmg="".01"" oriopen="".01""/>
+</anim>"));
+
+                File.AppendAllText(xmlPath, "</sp>");
+            }
         }
     }
 }
